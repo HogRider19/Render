dd1bc375daf56d9739a60267cd37260829e7acb7
Danil Dmitrenko <danilzzzg19@gmail.com>
2023-02-20 21:22:35 +0300

    Added utils!

---
+++ src/MathObjects/utils.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "vector.h"
+#include "math.h"
+
+
+/*
+    convertPlaneСoordToGlobal
+
+    Принимает координаты на плоскости (x, y), сама плоскость задана нормалью и принадлежащей
+    точкой (planenormal, planePoint <- в трехмерном пространстве), при этом planePoint считается
+    началом координат на плоскости, то есть x=0, y=0. Функция должна вернуть координаты
+    переданной точки (x, y) в трехмерном пространстве в виде вектора 
+*/
+
+Vector convertPlaneСoordToGlobal(double x, double y, Vector planeNormal, Vector planePoint);
